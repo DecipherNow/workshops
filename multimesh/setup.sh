e7b56cdd3cfaecc1854547ac0b2cd6c3df7b1c9b
kaitmore <kaitmore@gmail.com>
2019-11-17 12:01:45 -0500

    update ami build

--- multimesh/setup.sh
+++ multimesh/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 clear
 
 read -p "Docker Username: " username
@@ -10,28 +11,29 @@ export DOCKER_USER=$username
 export DOCKER_PASSWORD=$password
 
 echo "Starting Minikube..."
-minikube start --memory 6144 --cpus 4 --vm-driver=none >/dev/null 2>&1
+sudo minikube start --memory 6144 --cpus 4 --vm-driver=none >/dev/null
 
 # Dependencies
 echo "Downloading the Greymatter cli..."
-curl -u $DOCKER_USER:$DOCKER_PASSWORD https://nexus.production.deciphernow.com/repository/raw-hosted/greymatter/gm-cli/greymatter-v1.0.2.tar.gz --output greymatter-v1.0.2.tar.gz
+
+curl -s -u $DOCKER_USER:$DOCKER_PASSWORD https://nexus.production.deciphernow.com/repository/raw-hosted/greymatter/gm-cli/greymatter-v1.0.2.tar.gz --output greymatter-v1.0.2.tar.gz
 tar -xzf greymatter-v1.0.2.tar.gz
 mv greymatter.linux greymatter
 sudo mv greymatter /usr/local/bin
 source .profile
 
 echo "Installing Voyager Ingress..."
-curl -fsSL https://raw.githubusercontent.com/appscode/voyager/10.0.0/hack/deploy/voyager.sh | bash -s -- --provider=minikube >/dev/null 2>&1
+curl -sSL https://raw.githubusercontent.com/appscode/voyager/10.0.0/hack/deploy/voyager.sh | bash -s -- --provider=minikube >/dev/null
 
 # Installation
 echo "Installing Grey Matter..."
 kubectl create secret docker-registry docker.secret --docker-server="docker.production.deciphernow.com" --docker-username=$DOCKER_USER --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_USER
-kubectl apply -f template.yaml --validate=false >/dev/null 2>&1
+kubectl apply -f template.yaml --validate=false >/dev/null
 
 while [[ $(kubectl get pods --field-selector=status.phase=Running --output json | jq -j '.items | length') != "8" ]]; do
     clear
     echo "Waiting for pods ($(kubectl get pods --field-selector=status.phase=Running --output json | jq -j '.items | length')/8)"
-    kubectl get pods | grep Running
+    kubectl get pods | grep Running || true
     sleep 10
 done
 
@@ -39,6 +41,5 @@ clear
 
 echo "Exposing Edge..."
 
-minikube service voyager-edge >/dev/null 2>&1
-
+sudo minikube service voyager-edge >/dev/null
 echo "The mesh is ready at https://$PUBLIC_IP:30000 !"
